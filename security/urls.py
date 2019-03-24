a6f8203365ab1d931c37ffb65cdd1b7bf2475838
Matt Andrzejczuk <matt@jawn.it>
2019-03-23 23:59:56 -0400

    added missing files.

--- security/urls.py
+++ security/urls.py
@@ -10,5 +10,5 @@ app_name = "security"
 urlpatterns = [
     path('login', auth_views.LoginView.as_view(), name="login"),
     path('logout', auth_views.LogoutView.as_view(), name="logout"),
-    path('register', RegisterView.as_view(), name="register"),
+    path('register', RegisterView.as_view(success_url=""), name="register"),
 ]
--- security/views.py
+++ security/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 
-# Create your views here.
+
 class RegisterView(CreateView):
     template_name = 'user/register.html'
     form_class = UserCreationForm
-
